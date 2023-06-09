111e7230b6efd4876edf29ac1cd43089eb0714ff
Rian Souw <developer.rian@proton.me>
2023-06-10 03:27:37 +0700

    Add files via upload

---
+++ mager.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Made by Rian
+# Automate your repetitive tasks!
+sudo dnf update
+sudo dnf upgrade
+
+# Spotify Installation
+sudo dnf install -y flatpak && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Some tool installations
+sudo dnf install neofetch neovim docker htop fish
