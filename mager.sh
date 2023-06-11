973b947a0daa0e8321ac8c449be32a08130d7df9
Rian <developer.rian@proton.me>
2023-06-11 12:46:22 +0700

    delete "docker" word

--- mager.sh
+++ mager.sh
@@ -8,6 +8,6 @@ sudo dnf upgrade
 sudo dnf install -y flatpak && flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Some tool installations
-sudo dnf install neofetch neovim docker htop fish
+sudo dnf install neofetch neovim htop fish
 
 # I u have tried my config files and it doesn't work, please write a pull request or issues!
